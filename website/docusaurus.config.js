acae7a1526572d92070f275eeb0dd1ab6a623901
DigiPie <evan.it.sg@gmail.com>
2020-11-25 15:12:01 +0800

    feat: style landing page

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,102 +1,112 @@
+const internetProfiles = {
+  linkedin: {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/evanitsg/",
+  },
+  github: {
+    label: "GitHub",
+    href: "https://github.com/digipie",
+  },
+  email: {
+    label: "Email",
+    href: "mailto:evan.it.sg@gmail.com",
+  },
+  blog: {
+    label: "Blog",
+    to: "blog",
+  },
+  docs: {
+    label: "Docs",
+    to: "docs",
+  },
+  projects: {
+    label: "Projects",
+    to: "projects/",
+  },
+  resume: {
+    label: "Resume",
+    href: "https://github.com/DigiPie/Deedy-Resume/raw/master/resume.pdf",
+  },
+};
+
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "Evan Tay",
+  tagline:
+    "A Software Engineer and Cloud Engineer passionate about solving Meaningful Problems.",
+  url: "https://www.evantay.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.png",
+  organizationName: "digipie", // Usually your GitHub org/user name.
+  projectName: "kaya-folio", // Usually your repo name.
   themeConfig: {
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      title: "Evan Tay",
       items: [
+        { to: "blog", label: "Blog", position: "left" },
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          to: "projects/",
+          activeBasePath: "projects",
+          label: "Projects",
+          position: "left",
+        },
+        {
+          href: internetProfiles.resume.href,
+          label: "Resume",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: "Connect",
           items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
+            internetProfiles.linkedin,
+            internetProfiles.github,
+            internetProfiles.email,
           ],
         },
         {
-          title: 'Community',
+          title: "Discover",
           items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
+            internetProfiles.blog,
+            internetProfiles.docs,
+            internetProfiles.projects,
+            internetProfiles.resume,
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Last updated on ${new Date().toDateString()}`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: "https://github.com/DigiPie/kaya-folio/tree/main/website",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl: "https://github.com/DigiPie/kaya-folio/tree/main/website/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,80 +1,117 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeOpen } from "@fortawesome/free-regular-svg-icons";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
+import styles from "./styles.module.css";
+
+const socialLinks = [
+  {
+    faIcon: faLinkedinIn,
+    alt: "Linkedin",
+    url: "https://www.linkedin.com/in/evanitsg/",
+  },
+  {
+    faIcon: faGithub,
+    alt: "GitHub",
+    url: "https://github.com/digipie",
+  },
+  {
+    faIcon: faEnvelopeOpen,
+    alt: "Email",
+    url: "mailto:evan.it.sg@gmail.com",
+  },
+];
+
+function SocialLink({ faIcon, alt, url }) {
+  const formattedUrl = useBaseUrl(url);
+  return (
+    <li>
+      <a href={formattedUrl}>
+        <FontAwesomeIcon alt={alt} title={alt} icon={faIcon} size="2x" />
+      </a>
+    </li>
+  );
+}
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: "Full-Stack Software Engineer",
+    imageUrl: "img/fullstack.png",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        I was a Full-Stack Software Engineer Intern at{" "}
+        <a href="https://open.gov.sg/">Open Government Products</a> for Summer
+        2020. I am currently learning more about{" "}
+        <a href="https://reactjs.org/">ReactJS</a>.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: "Certified Cloud Engineer",
+    imageUrl: "img/cloud.png",
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        I am a{" "}
+        <a href="https://www.credential.net/a3645798-b27d-401a-a412-7f1c94ba7e6a">
+          Google Certified Associate Cloud Engineer
+        </a>{" "}
+        and{" "}
+        <a href="https://www.youracclaim.com/badges/681b95e4-178e-4d95-af41-dcdc6dbb9442/public_url">
+          AWS Certified Cloud Practitioner
+        </a>
+        . I am currently learning more about{" "}
+        <a href="https://www.udemy.com/certificate/UC-a6e7af40-8f26-4c13-846f-636b88ce326e/">
+          AWS CloudFormation
+        </a>{" "}
+        and <a href="http://terraform.io/">Hashicorp Terraform</a>.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--6", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <div className={styles.featureText}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <Layout description="Description will go into a meta tag in <head />">
+      <header className={clsx("hero", styles.heroBanner)}>
         <div className="container">
+          <img
+            src="img/profilepic.jpg"
+            style={{
+              borderRadius: "50%",
+              maxWidth: 128,
+            }}
+          />
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
+          <ul className={styles.socialLinkList}>
+            {socialLinks.map((props, idx) => (
+              <SocialLink key={idx} {...props} />
+            ))}
+          </ul>
         </div>
       </header>
       <main>
@@ -89,6 +126,9 @@ function Home() {
             </div>
           </section>
         )}
+        <p className="text--center">
+          <a href="http://www.freepik.com">Graphics designed by slidesgo / Freepik</a>
+        </p>
       </main>
     </Layout>
   );
