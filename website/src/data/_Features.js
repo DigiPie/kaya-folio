97c50c8ee89ce38e54130c5a904b9c38dd4cdda3
DigiPie <evan.it.sg@gmail.com>
2020-12-27 20:17:39 +0800

    refactor(landing): modify data structure

---
+++ website/src/data/_Features.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const features = [
+  {
+    title: "Full Stack Software Engineer",
+    imageUrl: "img/fullstack.png",
+    description: (
+      <>
+        I am currently working as a Full Stack Engineer at{" "}
+        <a href="https://padlet.com/">Padlet</a>. I am currently learning more
+        about <a href="https://reactjs.org/">ReactJS</a>.
+      </>
+    ),
+  },
+  {
+    title: "Certified Cloud Engineer",
+    imageUrl: "img/cloud.png",
+    description: (
+      <>
+        I am a{" "}
+        <a href="https://www.credential.net/a3645798-b27d-401a-a412-7f1c94ba7e6a">
+          Google Certified Associate Cloud Engineer
+        </a>{" "}
+        and{" "}
+        <a href="https://www.youracclaim.com/badges/681b95e4-178e-4d95-af41-dcdc6dbb9442/public_url">
+          AWS Certified Cloud Practitioner
+        </a>
+        .
+      </>
+    ),
+  },
+];
+
+export default features;
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -8,41 +8,7 @@ import Layout from "@theme/Layout";
 
 import styles from "./styles.module.css";
 import SocialLinks from "./components/_SocialLinks";
-
-const features = [
-  {
-    title: "Full Stack Software Engineer",
-    imageUrl: "img/fullstack.png",
-    description: (
-      <>
-        I am currently working as a Full Stack Engineer at{" "}
-        <a href="https://padlet.com/">Padlet</a>. I am currently learning more
-        about <a href="https://reactjs.org/">ReactJS</a>.
-      </>
-    ),
-  },
-  {
-    title: "Certified Cloud Engineer",
-    imageUrl: "img/cloud.png",
-    description: (
-      <>
-        I am a{" "}
-        <a href="https://www.credential.net/a3645798-b27d-401a-a412-7f1c94ba7e6a">
-          Google Certified Associate Cloud Engineer
-        </a>{" "}
-        and{" "}
-        <a href="https://www.youracclaim.com/badges/681b95e4-178e-4d95-af41-dcdc6dbb9442/public_url">
-          AWS Certified Cloud Practitioner
-        </a>
-        . I am currently learning more about{" "}
-        <a href="https://www.hashicorp.com/resources/getting-started-with-infrastructure-as-code-iac">
-          Infrastructure-as-Code
-        </a>
-        .
-      </>
-    ),
-  },
-];
+import features from "../data/_Features";
 
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
