4734e203119bb59f1238c304d1e2211a27381479
digipie <evan.it.sg@gmail.com>
2022-02-24 11:19:41 +0800

    fix: remove icon hotfix

--- website/src/pages/components/_SocialLinks.js
+++ website/src/pages/components/_SocialLinks.js
@@ -1,11 +1,4 @@
 import React from "react";
-
-// The following import prevents a Font Awesome icon server-side rendering bug,
-// where the icons flashes from a very large icon down to a properly sized one
-import "@fortawesome/fontawesome-svg-core/styles.css";
-// Prevent Font Awesome from adding its CSS since we already did it manually above
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false; /* eslint-disable import/first */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./styles.module.css";
 
