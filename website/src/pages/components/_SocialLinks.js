4600aa433eeb58185c11acb24c94a5955ee345d4
digipie <evan.it.sg@gmail.com>
2022-11-20 16:12:07 +0800

    Change social link colors

--- website/src/pages/components/_SocialLinks.js
+++ website/src/pages/components/_SocialLinks.js
@@ -8,7 +8,7 @@ import socialLinks from "../../data/_SocialLinks";
 function SocialLink({ faIcon, alt, url }) {
   return (
     <div>
-      <a href={url}>
+      <a href={url} className="text-primary-900 hover:text-primary-800">
         <FontAwesomeIcon alt={alt} title={alt} icon={faIcon} size="2x" />
       </a>
     </div>
